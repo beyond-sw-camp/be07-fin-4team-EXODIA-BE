3a62f06c7b7f9726d75522604794b4ac4cd4e007
suhyunpark <psh7492@gmail.com>
2024-09-24 14:08:25 +0900

    feat: Submit create

---
+++ src/main/java/com/example/exodia/submit/dto/SubmitSaveReqDto.java
@@ -0,0 +1,59 @@
+package com.example.exodia.submit.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.exodia.common.domain.DelYN;
+import com.example.exodia.submit.domain.Submit;
+import com.example.exodia.submit.domain.SubmitLine;
+import com.example.exodia.submit.domain.SubmitStatus;
+import com.example.exodia.user.domain.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SubmitSaveReqDto {
+
+	private String submitType;
+	private String contents;
+
+	private List<SubmitUserDto> submitUserDtos;
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	public static class SubmitUserDto {
+		private String userName;
+		private String position;
+	}
+
+
+	public Submit toEntity(User user) {
+		return Submit.builder()
+			.user(user)
+			.submitStatus(SubmitStatus.WAITING)
+			.submitType(this.getSubmitType())
+			.contents(this.getContents())
+			.submitLines(new ArrayList<>())
+			.department_id(user.getDepartment().getId())
+			.delYn(DelYN.N)
+			.build();
+	}
+
+	public SubmitLine toLineEntity(User user) {
+		return SubmitLine.builder()
+			.submitStatus(SubmitStatus.WAITING)
+			.userNum(user.getUserNum())
+			.department_id(user.getDepartment().getId())
+			.delYn(DelYN.N)
+			.build();
+	}
+
+}
--- src/main/java/com/example/exodia/submit/service/SubmitService.java
+++ src/main/java/com/example/exodia/submit/service/SubmitService.java
@@ -1,15 +1,75 @@
 package com.example.exodia.submit.service;
 
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.example.exodia.common.domain.DelYN;
+import com.example.exodia.position.domain.Position;
+import com.example.exodia.position.repository.PositionRepository;
+import com.example.exodia.submit.domain.Submit;
+import com.example.exodia.submit.domain.SubmitLine;
+import com.example.exodia.submit.domain.SubmitStatus;
+import com.example.exodia.submit.dto.SubmitSaveReqDto;
+import com.example.exodia.submit.dto.SubmitStatusUpdateDto;
+import com.example.exodia.submit.repository.SubmitLineRepository;
 import com.example.exodia.submit.repository.SubmitRepository;
+import com.example.exodia.user.domain.User;
+import com.example.exodia.user.repository.UserRepository;
 
 @Service
 public class SubmitService {
 
 	private final SubmitRepository submitRepository;
+	private final UserRepository userRepository;
+	private final PositionRepository positionRepository;
+	private final SubmitLineRepository submitLineRepository;
 
-	public SubmitService(SubmitRepository submitRepository) {
+	public SubmitService(SubmitRepository submitRepository, UserRepository userRepository,
+		PositionRepository positionRepository, SubmitLineRepository submitLineRepository) {
 		this.submitRepository = submitRepository;
+		this.userRepository = userRepository;
+		this.positionRepository = positionRepository;
+		this.submitLineRepository = submitLineRepository;
 	}
+
+	// 	결재라인 등록
+	public Submit createSubmit(SubmitSaveReqDto dto) {
+		Submit submit = new Submit();
+		for (SubmitSaveReqDto.SubmitUserDto userDto : dto.getSubmitUserDtos()) {
+			Position position = positionRepository.findByName(userDto.getPosition())
+				.orElseThrow(() -> new EntityNotFoundException("직급 정보가 존재하지 않습니다."));
+
+			User user = userRepository.findByNameAndPosition(userDto.getUserName(), position)
+				.orElseThrow(() -> new EntityNotFoundException("회원정보가 존재하지 않습니다."));
+
+			submit = dto.toEntity(user);
+			SubmitLine submitLine = dto.toLineEntity(user);
+
+			submit.getSubmitLines().add(submitLine);
+			submitLine.updateSubmit(submit);
+
+			submitRepository.save(submit);
+			submitLineRepository.save(submitLine);
+		}
+		return submit;
+	}
+
+	// 결재 상태 변경
+	public SubmitLine updateSubmit(SubmitStatusUpdateDto dto) {
+		String userNum = SecurityContextHolder.getContext().getAuthentication().getName();	// 사용자
+		SubmitLine submitLine = submitLineRepository.findByUserNumAndSubmitId(userNum, dto.getSubmitId())
+			.orElseThrow(() -> new EntityNotFoundException("결재 정보가 존재하지 않습니다."));
+
+		submitLine.updateStatus(dto.getStatus());
+		submitLineRepository.save(submitLine);
+		return submitLine;
+	}
+
+	// 모든 결제 상대에게 결재 받으면 submit의 submitStatus상태 변경
+
+
 }
