2ebb531815b5d0a0d48aa02fca7962a63cccf062
suhyunpark <psh7492@gmail.com>
2024-09-26 09:08:00 +0900

    fix: fix conflict RedisConfig

--- src/main/java/com/example/exodia/common/config/RedisConfig.java
+++ src/main/java/com/example/exodia/common/config/RedisConfig.java
@@ -1,13 +1,7 @@
 package com.example.exodia.common.config;
 
-<<<<<<< HEAD
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-=======
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
->>>>>>> 337b0fb (feat: Add documentRedisTempalte)
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
