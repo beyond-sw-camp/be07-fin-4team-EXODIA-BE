46684500c7e928f469d0eb4d609cabe3b9a58f8d
snikuz12 <snikuz12@naver.com>
2024-10-17 10:30:07 +0900

    fix : boardtagsrepository

--- src/main/java/com/example/exodia/board/repository/BoardTagsRepository.java
+++ src/main/java/com/example/exodia/board/repository/BoardTagsRepository.java
@@ -4,6 +4,6 @@ import com.example.exodia.board.domain.BoardTags;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // Tags 엔티티를 관리하는 JPA Repository
-public interface TagRepository extends JpaRepository<BoardTags, Long> {
+public interface BoardTagsRepository extends JpaRepository<BoardTags, Long> {
 
 }
