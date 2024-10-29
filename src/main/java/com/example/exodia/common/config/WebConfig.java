692c6a60ca40ad4a949fe2f5177f6f3f140ec354
suhyunpark <psh7492@gmail.com>
2024-10-30 06:08:28 +0900

    fix

--- src/main/java/com/example/exodia/common/config/WebConfig.java
+++ src/main/java/com/example/exodia/common/config/WebConfig.java
@@ -14,10 +14,10 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:8082");
-        config.addAllowedOrigin("http://localhost:8088");
-        config.addAllowedOrigin("http://localhost:8087");
-        config.addAllowedOrigin("http://localhost:4443");
+        config.addAllowedOrigin("http://exodiapot.xyz");
+        // config.addAllowedOrigin("http://localhost:8088");
+        // config.addAllowedOrigin("http://localhost:8087");
+        // config.addAllowedOrigin("http://localhost:4443");
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
