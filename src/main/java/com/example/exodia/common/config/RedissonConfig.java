75bee6442427eeed4610455ec6476c135120f3ad
leem5514 <ikaros0569@gmail.com>
2024-10-31 16:57:04 +0900

    fix redission

--- src/main/java/com/example/exodia/common/config/RedissonConfig.java
+++ src/main/java/com/example/exodia/common/config/RedissonConfig.java
@@ -1,26 +1,37 @@
-//package com.example.exodia.common.config;
-//
-//import org.redisson.Redisson;
-//import org.redisson.api.RedissonClient;
-//import org.redisson.config.Config;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class RedissonConfig {
-//
-//    @Value("${spring.data.redis.host}")
-//    private String redisHost;
-//
-//    @Value("${spring.data.redis.port}")
-//    private int redisPort;
-//
-//    @Bean
-//    public RedissonClient redissonClient() {
-//        Config config = new Config();
-//        config.useClusterServers()
-//                .addNodeAddress("redis://" + redisHost + ":" + redisPort);
-//        return Redisson.create(config);
-//    }
-//}
+package com.example.exodia.common.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.ClusterServersConfig;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class RedissonConfig {
+
+//    @Value("${spring.data.redis.hosts}")
+//    private List<String> redisNodes;
+//    @Value("${spring.data.redis.password}")
+//    private String redisPassword;
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+    @Bean
+
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setRetryAttempts(3)
+                .setRetryInterval(1500)
+                .setConnectTimeout(10000)
+                .setTimeout(3000);
+        return Redisson.create(config);
+    }
+}
