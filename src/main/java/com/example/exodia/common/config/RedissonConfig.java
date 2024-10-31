a8e5cb93ca600ba0d9f3ac8d49c0bf2c499887f2
leem5514 <ikaros0569@gmail.com>
2024-10-31 17:02:27 +0900

    fix

--- src/main/java/com/example/exodia/common/config/RedissonConfig.java
+++ src/main/java/com/example/exodia/common/config/RedissonConfig.java
@@ -2,36 +2,33 @@ package com.example.exodia.common.config;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
-import org.redisson.config.ClusterServersConfig;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class RedissonConfig {
 
-//    @Value("${spring.data.redis.hosts}")
-//    private List<String> redisNodes;
-//    @Value("${spring.data.redis.password}")
-//    private String redisPassword;
     @Value("${spring.data.redis.host}")
-    private String redisHost;
+    private String redisHost;  // Redis 클러스터 엔드포인트
 
     @Value("${spring.data.redis.port}")
     private int redisPort;
-    @Bean
 
+    @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer()
-                .setAddress("redis://" + redisHost + ":" + redisPort)
+
+        // 클러스터 서버 설정
+        config.useClusterServers()
+                .addNodeAddress("redis://" + redisHost + ":" + redisPort)
+                .setScanInterval(2000)
                 .setRetryAttempts(3)
                 .setRetryInterval(1500)
                 .setConnectTimeout(10000)
                 .setTimeout(3000);
+
         return Redisson.create(config);
     }
 }
