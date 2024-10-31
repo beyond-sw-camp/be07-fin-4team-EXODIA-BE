035290caf89fb3b71dfccf30abd9718ae850f04e
leem5514 <ikaros0569@gmail.com>
2024-10-31 17:26:00 +0900

    fix

--- src/main/java/com/example/exodia/common/config/RedissonConfig.java
+++ src/main/java/com/example/exodia/common/config/RedissonConfig.java
@@ -1,5 +1,6 @@
 package com.example.exodia.common.config;
 
+import jakarta.annotation.PostConstruct;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.ClusterServersConfig;
@@ -16,11 +17,15 @@ public class RedissonConfig {
     @Value("${spring.data.redis.nodes}")
     private List<String> redisNodes;
 
+    @PostConstruct
+    public void checkRedisNodes() {
+        System.out.println("Redis Nodes: " + redisNodes);
+    }
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
 
-        // 클러스터 서버 설정
         ClusterServersConfig clusterConfig = config.useClusterServers()
                 .setScanInterval(2000)
                 .setRetryAttempts(3)
@@ -28,7 +33,6 @@ public class RedissonConfig {
                 .setConnectTimeout(10000)
                 .setTimeout(3000);
 
-        // 모든 노드 주소 추가
         redisNodes.forEach(node -> clusterConfig.addNodeAddress("redis://" + node));
 
         return Redisson.create(config);
