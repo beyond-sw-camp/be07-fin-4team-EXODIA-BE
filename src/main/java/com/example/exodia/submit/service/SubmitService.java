6e4e2feffa5407ae77be99702f7b97986f4a1560
suhyunpark <psh7492@gmail.com>
2024-11-03 13:19:47 +0900

    fix: 결재라인 알림 수정

--- src/main/java/com/example/exodia/submit/service/SubmitService.java
+++ src/main/java/com/example/exodia/submit/service/SubmitService.java
@@ -88,6 +88,7 @@ public class SubmitService {
 		User submitUser = userRepository.findByUserNum(userNum)
 			.orElseThrow(() -> new EntityNotFoundException("회원정보가 존재하지 않습니다."));
 
+		String receiverUserNum = null;
 		if (dto.getSubmitUserDtos() == null) {
 			throw new IOException("결재 라인을 등록하십시오.");
 		}
@@ -103,6 +104,9 @@ public class SubmitService {
 				.orElseThrow(() -> new EntityNotFoundException("회원정보가 존재하지 않습니다."));
 
 			submitLine = dto.toLineEntity(user);
+			if (receiverUserNum == null) {
+				receiverUserNum = submitLine.getUserNum();
+			}
 
 			submit.getSubmitLines().add(submitLine);
 			submitLine.updateSubmit(submit);
@@ -110,6 +114,11 @@ public class SubmitService {
 
 		submitRepository.save(submit);
 		submitLineRepository.save(submitLine);
+
+
+		kafkaProducer.sendSubmitNotification("submit-events", dto.getSubmitUserDtos().get(0).getUserName(),
+			receiverUserNum,
+			LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM.dd")));
 		return submit;
 	}
 
