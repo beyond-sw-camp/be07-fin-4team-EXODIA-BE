e76a0912119763984dea479a28c40ab11f107147
snikuz12 <snikuz12@naver.com>
2024-11-05 02:13:42 +0900

    fix

--- src/main/java/com/example/exodia/common/config/WebConfig.java
+++ src/main/java/com/example/exodia/common/config/WebConfig.java
@@ -14,10 +14,10 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-      
-        config.addAllowedOriginPattern("https://*.exodiapot.xyz");
-//        config.addAllowedOriginPattern("http://localhost:[8082,8087,8088,4443]");
-        config.addAllowedOriginPattern("https://openvidu.exodiapot.xyz");
+
+//        config.addAllowedOriginPattern("https://*.exodiapot.xyz");
+        config.addAllowedOriginPattern("http://localhost:[8082,8087,8088,4443]");
+//        config.addAllowedOriginPattern("https://openvidu.exodiapot.xyz");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
