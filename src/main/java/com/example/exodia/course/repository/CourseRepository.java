534b35d32d69f49fbbdaaff1c36f8f96ec842cc7
leem5514 <ikaros0569@gmail.com>
2024-11-06 02:02:42 +0900

    fix

--- src/main/java/com/example/exodia/course/repository/CourseRepository.java
+++ src/main/java/com/example/exodia/course/repository/CourseRepository.java
@@ -5,9 +5,15 @@ import com.example.exodia.course.domain.Course;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
     Page<Course> findByDelYn(DelYN delYn, Pageable pageable);
+
+    @Query("SELECT c FROM Course c WHERE c.startTime = :startTime AND c.delYn = :delYn")
+    List<Course> findCoursesStartingAt(@Param("startTime") LocalDateTime startTime, @Param("delYn") DelYN delYn);
 }
