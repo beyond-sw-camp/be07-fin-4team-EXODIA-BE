134e694f94e9c66849cd38f17a5873990c4aef1e
leem5514 <ikaros0569@gmail.com>
2024-11-06 05:17:03 +0900

    Fix

--- src/main/java/com/example/exodia/common/config/SecurityConfig.java
+++ src/main/java/com/example/exodia/common/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("**").permitAll()
+                        .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
